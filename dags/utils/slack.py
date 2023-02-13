24c090e8bf0cda9a6c213fb2e4163d50480610e6
aDrz <adrien.nouvellet@gmail.com>
2023-02-13 18:16:51 +0100

    feat: backfill

--- dags/utils/slack.py
+++ dags/utils/slack.py
@@ -38,7 +38,7 @@ def slack_fail_alert(context):
     return slack_alert.execute(context=context)
 
 
-def slack_success_alert(ti):
+def slack_success_alert(ds, ti):
     """Adapted from https://medium.com/datareply/integrating-slack-alerts-in-airflow-c9dcd155105
     Sends message to a slack channel.
        If you want to send it to a "user" -> use "@user",
@@ -50,11 +50,8 @@ def slack_success_alert(ti):
     channel = BaseHook.get_connection(SLACK_CONN_ID).login
     slack_msg = f"""
         :coche_blanche: Task Success.
-        *Task*: {context.get('task_instance').task_id}
-        *Dag*: {context.get('task_instance').dag_id}
-        *Execution Time*: {convert_datetime(context.get('execution_date'))}
+        *Execution Time*: {ds}
         *Demo Urls*: {urls_demo}
-        <{context.get('task_instance').log_url}|*Logs*>
     """
     # see https://towardsdatascience.com/integrating-docker-airflow-with-slack-to-get-daily-reporting-c462e7c8828a#:~:text=The%20Slack%20Webhook%20Operator%20can,some%20trigger%20condition%20is%20met. # noqa
     slack_alert = SlackWebhookOperator(
@@ -66,4 +63,4 @@ def slack_success_alert(ti):
         http_conn_id=SLACK_CONN_ID,
     )
 
-    return slack_alert.execute(context=context)
+    return slack_alert
