aa0abeb14c91806ce6e9ef20f53d36ad5240b803
aDrz <adrien.nouvellet@gmail.com>
2023-02-13 10:05:29 +0100

    change start_date to february

--- dags/csgo_download.py
+++ dags/csgo_download.py
@@ -179,7 +179,7 @@ dag = DAG(
     schedule_interval="0 1 * * *",
     dagrun_timeout=datetime.timedelta(minutes=120 * 2),
     description="use case of python operator in airflow",
-    start_date=datetime.datetime(2023, 1, 1),
+    start_date=datetime.datetime(2023, 2, 1),
     catchup=True,
 )
 
