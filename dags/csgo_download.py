e1e237b56bec385f1176fb3c033aa1899b849f33
aDrz <adrien.nouvellet@gmail.com>
2023-02-18 17:12:18 +0100

    backfill

--- dags/csgo_download.py
+++ dags/csgo_download.py
@@ -185,7 +185,7 @@ dag = DAG(
     schedule_interval="0 1 * * *",
     dagrun_timeout=datetime.timedelta(minutes=120 * 2),
     description="use case of python operator in airflow",
-    start_date=datetime.datetime(2021, 1, 1),
+    start_date=datetime.datetime(2020, 1, 1),
     catchup=True,
     max_active_tasks=4,
 )
